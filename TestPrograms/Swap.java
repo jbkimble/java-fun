31e68905f578d87d87a33fdf89beae926779a3a0
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-03 13:58:14 -0700

    Super basic swap example

---
+++ TestPrograms/Swap.java
@@ -0,0 +1,9 @@
+public class Swap {
+  public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    int c = a;
+    a=b;
+    b=c;
+  }
+}
