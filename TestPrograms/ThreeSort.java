86eaf406ff280dfaee8bc7fea65e75b7dee34a3d
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-03 13:19:38 -0700

    Finished three sort problem

---
+++ TestPrograms/ThreeSort.java
@@ -0,0 +1,20 @@
+public class ThreeSort {
+  public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    int c = Integer.parseInt(args[2]);
+    int smallest = Math.min(a, Math.min(b, c));
+    int largest = Math.max(a, Math.max(b, c));
+    int middle;
+    
+    if ((smallest == a || smallest == b) && (largest == a || largest == b)) {
+      middle = c;
+    } else if ((smallest == c || smallest == b) && (largest == c || largest == b)) {
+      middle = a;
+    } else {
+      middle = b;
+    }
+
+    System.out.println(smallest + " " + middle + " " + largest);
+  }
+}
