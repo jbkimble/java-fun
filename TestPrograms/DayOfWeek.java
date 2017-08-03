cb9fcd9f05f20205be485527d1ede2f1acdc4cb9
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-03 12:49:25 -0700

    Add spaces for clarity

--- TestPrograms/DayOfWeek.java
+++ TestPrograms/DayOfWeek.java
@@ -3,11 +3,13 @@ public class DayOfWeek {
     int month = Integer.parseInt(args[0]);
     int day = Integer.parseInt(args[1]);
     int year = Integer.parseInt(args[2]);
-    int dayOfWeek;
+    int dayOfWeek
+
     int temp = year - (14-month)/12;
     temp = temp + temp/4 - temp/100 + temp/400;
     int temp2 = month + 12 *((14 - month)/12) -2;
     dayOfWeek = (day + temp + (31*(temp2))/12)%7;
+    
     System.out.println("The day of the week is: " + dayOfWeek);
   }
 }
